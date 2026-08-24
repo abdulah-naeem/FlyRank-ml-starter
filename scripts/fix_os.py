ecb80a434fff164c2b4e003fd8529527a75564a4
Abdulah Naeem <abdulah.naeem@example.com>
2026-08-24 13:01:08 +0500

    Create fix_os.py

---
+++ scripts/fix_os.py
@@ -0,0 +1,14 @@
+﻿import json
+
+notebook_path = 'd:/Internships/Internship - FlyrankAI/Week 1/FlyRank-ml-starter/work/notebooks/w04_baseline_score.ipynb'
+with open(notebook_path, 'r', encoding='utf-8') as f:
+    nb = json.load(f)
+
+# Insert import os into Cell 4
+if "import os\n" not in nb['cells'][4]['source']:
+    nb['cells'][4]['source'].insert(0, "import os\n")
+
+with open(notebook_path, 'w', encoding='utf-8') as f:
+    json.dump(nb, f, indent=1)
+
+print("Added import os to Cell 4")
